e8ae21303a57765ec6ce4926c4c5c0bbbe87bc42
Andrew Serputko <ykozlov@hmstn.com>
2013-07-09 17:43:52 +0300

    Init project

---
+++ test/spec/my-test.js
@@ -0,0 +1,26 @@
+/*global describe, it */
+'use strict';
+describe('Tests for AAAA', function () {
+	var Model;
+	require([
+		'jquery',
+		'underscore',
+		'backbone',
+		'models/application-model'
+	], function($, _, Backbone, Unit) {
+		debugger;Model =  Unit
+	});
+
+	beforeEach(function () {
+		debugger
+		this.model = new Model();
+	});
+
+	it('should run here few assertions1', function () {
+		debugger; this.model;
+	});
+	it('should run here few assertions2');
+	it('should run here few assertions3');
+
+	
+});
