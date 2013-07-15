e42329344f975d8d037dbddaa9ff0ee181cc3ede
Andrew Serputko <aserputko@gmail.com>
2013-07-15 23:26:04 +0300

    bread-unit-collection-spec: should recognize query with upper and lower cases

--- test/spec/collections/bread-unit-collection-spec.js
+++ test/spec/collections/bread-unit-collection-spec.js
@@ -32,5 +32,26 @@ require(['collections/bread-unit-collection'], function (BreadUnitCollection) {
 			result = this.collection.filterByName('');
 			expect(result.length).to.equal(3);
         });
+
+        it('should recognize query with upper and lower cases', function () {
+			var result;
+			this.collection.add([{name: 'apple'}, {name: 'APPLE'}, {name: 'ApPlE'}]);
+			this.collection.add([{name: 'banana'}, {name: 'ORANGE'}]);
+
+			result = this.collection.filterByName('apple');
+			expect(result.length).to.equal(3);
+
+			result = this.collection.filterByName('APPLE');
+			expect(result.length).to.equal(3);
+
+			result = this.collection.filterByName('ApPlE');
+			expect(result.length).to.equal(3);
+
+			result = this.collection.filterByName('BANANA');
+			expect(result.length).to.equal(1);
+
+			result = this.collection.filterByName('orange');
+			expect(result.length).to.equal(1);
+        });
     });
 });
